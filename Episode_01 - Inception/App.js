d1a5920214cc6cb21d3cff8ee1f253d913e4c1d0
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2024-01-23 03:12:12 +0530

    init

--- Episode_01 - Inception/App.js
+++ Episode_01 - Inception/App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement("h1", {}, "Hello World!");
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+
+// parent is a js Object not html code
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// render method will take the obj created by React.createElement and convert it to HTML code and render it
+root.render(parent)
