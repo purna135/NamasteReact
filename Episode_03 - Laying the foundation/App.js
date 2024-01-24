bd47f34e9b8998ec5419fa59247a01357fde4c0e
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2024-01-24 23:57:34 +0530

    inital code

---
+++ Episode_03 - Laying the foundation/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// This is a React Element
+const headind = <h1>React Element</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headind)
