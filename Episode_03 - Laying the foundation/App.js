88f70ef99f485d4a13b9d983638363ebe12e1816
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2024-01-25 00:25:02 +0530

    React component

--- Episode_03 - Laying the foundation/App.js
+++ Episode_03 - Laying the foundation/App.js
@@ -1,8 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // This is a React Element
-const headind = <h1>React Element</h1>
+const heading = <h1>React Element</h1>;
+
+//This is React Component
+const Title = () => <h1>Title</h1>;
+const x = 100;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {x + 123}
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">React Component</h1>
+    {heading}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(headind)
+// root.render(HeadingComponent()); // this will also work
+root.render(<HeadingComponent />);
